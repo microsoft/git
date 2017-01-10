2130c0f6a751e05e9f4061d0f1a5ded8e0ef6f13
Ben Peart <Ben.Peart@microsoft.com>
2017-01-10 18:47:14 +0000

    gvfs: allow "virtualizing" objects
    The idea is to allow blob objects to be missing from the local repository,
    and to load them lazily on demand.
    
    After discussing this idea on the mailing list, we will rename the feature
    to "lazy clone" and work more on this.
    
    Signed-off-by: Ben Peart <Ben.Peart@microsoft.com>
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- odb.c
+++ odb.c
@@ -6,7 +6,9 @@
 #include "dir.h"
 #include "environment.h"
 #include "gettext.h"
+#include "gvfs.h"
 #include "hex.h"
+#include "hook.h"
 #include "khash.h"
 #include "lockfile.h"
 #include "loose.h"
@@ -26,6 +28,7 @@
 #include "submodule.h"
 #include "tmp-objdir.h"
 #include "trace2.h"
+#include "trace.h"
 #include "write-or-die.h"
 
 KHASH_INIT(odb_path_map, const char * /* key: odb_path */,
@@ -654,6 +657,20 @@ void disable_obj_read_lock(void)
 	pthread_mutex_destroy(&obj_read_mutex);
 }
 
+static int run_read_object_hook(struct repository *r, const struct object_id *oid)
+{
+	struct run_hooks_opt opt = RUN_HOOKS_OPT_INIT;
+	int ret;
+	uint64_t start;
+
+	start = getnanotime();
+	strvec_push(&opt.args, oid_to_hex(oid));
+	ret = run_hooks_opt(r, "read-object", &opt);
+	trace_performance_since(start, "run_read_object_hook");
+
+	return ret;
+}
+
 int fetch_if_missing = 1;
 
 static int register_all_submodule_sources(struct object_database *odb)
@@ -680,6 +697,7 @@ static int do_oid_object_info_extended(struct object_database *odb,
 	const struct cached_object *co;
 	const struct object_id *real = oid;
 	int already_retried = 0;
+	int tried_hook = 0;
 
 	if (flags & OBJECT_INFO_LOOKUP_REPLACE)
 		real = lookup_replace_object(odb->repo, oid);
@@ -687,6 +705,7 @@ static int do_oid_object_info_extended(struct object_database *odb,
 	if (is_null_oid(real))
 		return -1;
 
+retry:
 	co = find_cached_object(odb, real);
 	if (co) {
 		if (oi) {
@@ -723,6 +742,11 @@ static int do_oid_object_info_extended(struct object_database *odb,
 			odb_reprepare(odb->repo->objects);
 			if (!packfile_store_read_object_info(odb->packfiles, real, oi, flags))
 				return 0;
+			if (gvfs_virtualize_objects(odb->repo) && !tried_hook) {
+				tried_hook = 1;
+				if (!run_read_object_hook(odb->repo, oid))
+					goto retry;
+			}
 		}
 
 		/*
