cae47dc75adfe30c5f0f0c5e8dd8b64793a31148
Johannes Schindelin <johasc@microsoft.com>
2017-09-08 11:32:43 +0200

    sha1_file: when writing objects, skip the read_object_hook
    If we are going to write an object there is no use in calling
    the read object hook to get an object from a potentially remote
    source.  We would rather just write out the object and avoid the
    potential round trip for an object that doesn't exist.
    
    This change adds a flag to the check_and_freshen() and
    freshen_loose_object() functions' signatures so that the hook
    is bypassed when the functions are called before writing loose
    objects. The check for a local object is still performed so we
    don't overwrite something that has already been written to one
    of the objects directories.
    
    Based on a patch by Kevin Willford.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- odb/source-loose.c
+++ odb/source-loose.c
@@ -575,7 +575,8 @@ out:
 }
 
 static int odb_source_loose_freshen_object(struct odb_source *source,
-					   const struct object_id *oid)
+					   const struct object_id *oid,
+					   int skip_virtualized_objects)
 {
 	struct odb_source_loose *loose = odb_source_loose_downcast(source);
 	static struct strbuf path = STRBUF_INIT;
@@ -584,7 +585,8 @@ static int odb_source_loose_freshen_object(struct odb_source *source,
 	odb_loose_path(loose, &path, oid);
 retry:
 	ret = check_and_freshen_file(path.buf, 1);
-	if (!ret && gvfs_virtualize_objects(source->odb->repo) && !tried_hook) {
+	if (!ret && gvfs_virtualize_objects(source->odb->repo) &&
+	    !skip_virtualized_objects && !tried_hook) {
 		tried_hook = 1;
 		if (!read_object_process(source->odb->repo, oid))
 			goto retry;
@@ -625,7 +627,7 @@ static int odb_source_loose_write_object(struct odb_source *source,
 	 * it out into .git/objects/??/?{38} file.
 	 */
 	write_object_file_prepare(algo, buf, len, type, oid, hdr, &hdrlen);
-	if (odb_freshen_object(source->odb, oid))
+	if (odb_freshen_object(source->odb, oid, 1))
 		return 0;
 	if (write_loose_object(loose, oid, hdr, hdrlen, buf, len, 0, flags))
 		return -1;
