dd66d1eabc4291adff5c6cdcbcfbaff95193a3a9
Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
2018-07-29 12:33:03 +0200

    unpack-trees.c: add performance tracing
    We're going to optimize unpack_trees() a bit in the following
    patches. Let's add some tracing to measure how long it takes before
    and after. This is the baseline ("git checkout -" on gcc.git, 80k
    files on worktree)
    
        0.018239226 s: read cache .git/index
        0.052541655 s: preload index
        0.001537598 s: refresh index
        0.168167768 s: unpack trees
        0.002897186 s: update worktree after a merge
        0.131661745 s: repair cache-tree
        0.075389117 s: write index, changed mask = 2a
        0.111702023 s: unpack trees
        0.000023245 s: update worktree after a merge
        0.111793866 s: diff-index
        0.587933288 s: git command: /home/pclouds/w/git/git checkout -
    
    Signed-off-by: Nguyễn Thái Ngọc Duy <pclouds@gmail.com>

--- unpack-trees.c
+++ unpack-trees.c
@@ -353,6 +353,7 @@ static int check_updates(struct unpack_trees_options *o)
 	struct progress *progress = NULL;
 	struct index_state *index = &o->result;
 	struct checkout state = CHECKOUT_INIT;
+	uint64_t start = getnanotime();
 	int i;
 
 	state.force = 1;
@@ -424,6 +425,7 @@ static int check_updates(struct unpack_trees_options *o)
 	errs |= finish_delayed_checkout(&state);
 	if (o->update)
 		git_attr_set_direction(GIT_ATTR_CHECKIN, NULL);
+	trace_performance_since(start, "update worktree after a merge");
 	return errs != 0;
 }
 
@@ -1293,6 +1295,7 @@ int unpack_trees(unsigned len, struct tree_desc *t, struct unpack_trees_options 
 	int i, ret;
 	static struct cache_entry *dfc;
 	struct exclude_list el;
+	uint64_t start = getnanotime();
 
 	if (len > MAX_UNPACK_TREES)
 		die("unpack_trees takes at most %d trees", MAX_UNPACK_TREES);
@@ -1445,6 +1448,7 @@ int unpack_trees(unsigned len, struct tree_desc *t, struct unpack_trees_options 
 			goto done;
 		}
 	}
+	trace_performance_since(start, "unpack trees");
 
 	ret = check_updates(o) ? (-2) : 0;
 	if (o->dst_index) {
