3134fa9d1208c140820a77f984f630ec8c24146f
Jeff Hostetler <jeffhost@microsoft.com>
2019-09-24 14:31:10 -0400

    sha1-file: add function to update existing loose object cache
    Create a function to add a new object to the loose object cache
    after the existing odb/xx/ directory was scanned.
    
    This will be used in a later commit to keep the loose object
    cache fresh after dynamically fetching an individual object
    and without requiring the odb/xx/ directory to be rescanned.
    
    Signed-off-by: Jeff Hostetler <jeffhost@microsoft.com>

--- object-file.h
+++ object-file.h
@@ -38,6 +38,14 @@ int odb_source_loose_write_stream(struct odb_source_loose *source,
 				  struct object_id *oid);
 
 /*
+ * Add a new object to the loose object cache (possibly after the
+ * cache was populated).  This might be used after dynamically
+ * fetching a missing object.
+ */
+void odb_source_loose_cache_add_new_oid(struct odb_source *source,
+				 const struct object_id *oid);
+
+/*
  * Put in `buf` the name of the file in the local object database that
  * would be used to store a loose object with the specified oid.
  */
