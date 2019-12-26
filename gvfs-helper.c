e6abb7bf81067f6260568b2c183a63eb148024b1
Derrick Stolee <dstolee@microsoft.com>
2019-12-26 10:20:24 -0500

    gvfs-helper: retry when creating temp files
    When we create temp files for downloading packs, we use a name
    based on the current timestamp. There is no randomness in the
    name, so we can have collisions in the same second.
    
    Retry the temp pack names using a new "-<retry>" suffix to the
    name before the ".temp".
    
    Signed-off-by: Derrick Stolee <dstolee@microsoft.com>

--- gvfs-helper.c
+++ gvfs-helper.c
@@ -1677,6 +1677,7 @@ static void my_create_tempfile(
 	struct strbuf buf = STRBUF_INIT;
 	int len_tp;
 	enum scld_error scld;
+	int retries;
 
 	gh__response_status__zero(status);
 
@@ -1725,7 +1726,15 @@ static void my_create_tempfile(
 		goto cleanup;
 	}
 
+	retries = 0;
 	*t1 = repo_create_tempfile(repo, buf.buf);
+	while (!*t1 && retries < 5) {
+		retries++;
+		strbuf_setlen(&buf, len_tp);
+		strbuf_addf(&buf, "%s-%d.%s", basename.buf, retries, suffix1);
+		*t1 = repo_create_tempfile(repo, buf.buf);
+	}
+
 	if (!*t1) {
 		strbuf_addf(&status->error_message,
 			    "could not create tempfile: '%s'",
@@ -1747,6 +1756,13 @@ static void my_create_tempfile(
 		strbuf_addf(  &buf, "%s.%s", basename.buf, suffix2);
 
 		*t2 = repo_create_tempfile(repo, buf.buf);
+		while (!*t2 && retries < 5) {
+			retries++;
+			strbuf_setlen(&buf, len_tp);
+			strbuf_addf(&buf, "%s-%d.%s", basename.buf, retries, suffix2);
+			*t2 = repo_create_tempfile(repo, buf.buf);
+		}
+
 		if (!*t2) {
 			strbuf_addf(&status->error_message,
 				    "could not create tempfile: '%s'",
