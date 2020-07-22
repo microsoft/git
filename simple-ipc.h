464576f060e02fe6d1e73f3b0ae8a621c2202861
Jeff Hostetler <jeffhost@microsoft.com>
2020-07-22 06:08:35 -0400

    fixup: simple-ipc.h
    Signed-off-by: Jeff Hostetler <jeffhost@microsoft.com>

--- simple-ipc.h
+++ simple-ipc.h
@@ -11,29 +11,101 @@
 
 #ifdef SUPPORTS_SIMPLE_IPC
 
-enum IPC_ACTIVE_STATE {
-	IPC_STATE__ACTIVE = 0,
-	IPC_STATE__NOT_ACTIVE = 1,
-	IPC_STATE__INVALID_PATH = 2,
+enum ipc_active_state {
+	/*
+	 * The pipe/socket exists and the daemon is waiting for connections.
+	 */
+	IPC_STATE__LISTENING = 0,
+
+	/*
+	 * The pipe/socket exists, but the daemon is not listening.
+	 * Perhaps it is very busy.
+	 * Perhaps the daemon died without deleting the path.
+	 * Perhaps it is shutting down and draining existing clients.
+	 * Perhaps it is dead, but other clients are lingering and
+	 * still holding a reference to the pathname.
+	 */
+	IPC_STATE__NOT_LISTENING,
+
+	/*
+	 * The requested pathname is bogus and no amount of retries
+	 * will fix that.
+	 */
+	IPC_STATE__INVALID_PATH,
+
+	/*
+	 * The requested pathname is not found.  This usually means
+	 * that there is no daemon present.
+	 */
+	IPC_STATE__PATH_NOT_FOUND,
+
+	IPC_STATE__OTHER_ERROR,
 };
 
-/*
- * Inspect the filesystem to determine if a server is running on this
- * named pipe or socket (without actually sending a message) by testing
- * the availability and/or existence of the pipe or socket.
- */
-enum IPC_ACTIVE_STATE ipc_is_active(const char *path);
+struct ipc_client_connect_options {
+	/*
+	 * Spin under timeout if the server is running but can't
+	 * accept our connection yet.  This should always be set
+	 * unless you just want to poke the server and see if it
+	 * is alive.
+	 */
+	unsigned int wait_if_busy :1;
+
+	/*
+	 * Spin under timeout if the pipe/socket is not yet present
+	 * on the file system.  This is useful if we just started
+	 * the service and need to wait for it to become ready.
+	 */
+	unsigned int wait_if_not_found :1;
+};
+
+#define IPC_CLIENT_CONNECT_OPTIONS_INIT { \
+	.wait_if_busy = 0, \
+	.wait_if_not_found = 0, \
+}
 
 /*
- * Used by the client to synchronously send a message to the server and
- * receive a response.
+ * Determine if a server is listening on this named pipe or socket using
+ * platform-specific logic.  This might just probe the filesystem or it
+ * might make a trivial connection to the server using this pathname.
+ */
+enum ipc_active_state ipc_get_active_state(const char *path);
+
+/*
+ * Try to connect to the daemon on the named pipe or socket.
+ *
+ * Returns IPC_STATE__LISTENING (and an fd) when connected.
+ *
+ * Otherwise, returns info to help decide whether to retry or to
+ * spawn/respawn the server.
+ */
+enum ipc_active_state ipc_client_try_connect(
+	const char *path,
+	const struct ipc_client_connect_options *options,
+	int *pfd);
+
+/*
+ * Used by the client to synchronously send and receive a message with
+ * the server on the provided fd.
  *
  * Returns 0 when successful.
  *
  * Calls error() and returns non-zero otherwise.
  */
-int ipc_client_send_command(const char *path, const char *message,
-			    struct strbuf *answer);
+int ipc_client_send_command_to_fd(int fd, const char *message,
+				  struct strbuf *answer);
+
+/*
+ * Used by the client to synchronously connect and send and receive a
+ * message to the server listening at the given path.
+ *
+ * Returns 0 when successful.
+ *
+ * Calls error() and returns non-zero otherwise.
+ */
+int ipc_client_send_command(const char *path,
+			    const struct ipc_client_connect_options *options,
+			    const char *message, struct strbuf *answer);
 
 struct ipc_server_reply_data;
 
