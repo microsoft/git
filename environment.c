e2c454df31f21e9db72398ff45198ce9f7844278
Jeff Hostetler <jeffhost@microsoft.com>
2021-03-05 21:20:26 +0100

    fsmonitor: config settings are repository-specific
    Move FSMonitor config settings to a new `struct fsmonitor_settings`
    structure.  Add a lazily-loaded pointer to `struct repo_settings`.
    Create `fsm_settings__get_*()` getters to lazily look up fsmonitor-
    related config settings.
    
    Get rid of the `core_fsmonitor` global variable, and add support for
    the new `core.useBuiltinFSMonitor` config setting.  Move config code
    to lookup the existing `core.fsmonitor` value to `fsmonitor-settings.[ch]`.
    
    The `core_fsmonitor` global variable was used to store the pathname to
    the FSMonitor hook and it was used as a boolean to see if FSMonitor
    was enabled.  This dual usage will lead to confusion when we add
    support for a builtin FSMonitor based on IPC, since the builtin
    FSMonitor doesn't need the hook pathname.
    
    Replace the boolean usage with an `enum fsmonitor_mode` to represent
    the state of FSMonitor.  And only set the pathname when in HOOK mode.
    
    Signed-off-by: Jeff Hostetler <jeffhost@microsoft.com>

--- environment.c
+++ environment.c
@@ -90,7 +90,6 @@ int protect_ntfs = PROTECT_NTFS_DEFAULT;
 int core_use_gvfs_helper;
 const char *gvfs_cache_server_url;
 struct strbuf gvfs_shared_cache_pathname = STRBUF_INIT;
-const char *core_fsmonitor;
 
 /*
  * The character that begins a commented line in user-editable file
