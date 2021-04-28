012c2f55a82c1291fa573b7be1465d2dd484a039
Johannes Schindelin <johannes.schindelin@gmx.de>
2021-04-28 22:19:17 +0200

    scalar: generate usage string dynamically
    The subcommand list was somewhat stale; With this change, it cannot go
    stale anymore.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- contrib/scalar/scalar.c
+++ contrib/scalar/scalar.c
@@ -13,11 +13,6 @@
 #include "dir.h"
 #include "json-parser.h"
 
-static const char scalar_usage[] =
-	N_("scalar <command> [<options>]\n\n"
-	   "Commands: clone, config, diagnose, list\n"
-	   "\tregister, run, unregister");
-
 static int is_unattended(void) {
 	return git_env_bool("Scalar_UNATTENDED", 0);
 }
@@ -1075,20 +1070,25 @@ struct {
 
 int cmd_main(int argc, const char **argv)
 {
+	struct strbuf scalar_usage = STRBUF_INIT;
 	int i;
 
-	if (argc < 2)
-		usage(scalar_usage);
+	if (argc > 1) {
+		argv++;
+		argc--;
 
-	argv++;
-	argc--;
+		for (i = 0; builtins[i].name; i++)
+			if (!strcmp(builtins[i].name, argv[0])) {
+				if (builtins[i].needs_git_repo)
+					setup_git_directory();
+				return builtins[i].fn(argc, argv);
+			}
+	}
 
+	strbuf_addstr(&scalar_usage,
+		      N_("scalar <command> [<options>]\n\nCommands:\n"));
 	for (i = 0; builtins[i].name; i++)
-		if (!strcmp(builtins[i].name, argv[0])) {
-			if (builtins[i].needs_git_repo)
-				setup_git_directory();
-			return builtins[i].fn(argc, argv);
-		}
+		strbuf_addf(&scalar_usage, "\t%s\n", builtins[i].name);
 
-	usage(scalar_usage);
+	usage(scalar_usage.buf);
 }
