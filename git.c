f8208e9171bb8f083a295a4a70848fa63c95cacf
Matthew John Cheetham <mjcheetham@outlook.com>
2025-03-06 11:16:04 +0000

    worktree: remove special case GVFS cmd blocking
    Replace the special casing of the `worktree` command being blocked on
    VFS-enabled repos with the new `BLOCK_ON_VFS_ENABLED` flag.
    
    Signed-off-by: Matthew John Cheetham <mjcheetham@outlook.com>

--- git.c
+++ git.c
@@ -729,7 +729,7 @@ static struct cmd_struct commands[] = {
 #ifndef WITH_BREAKING_CHANGES
 	{ "whatchanged", cmd_whatchanged, RUN_SETUP },
 #endif
-	{ "worktree", cmd_worktree, RUN_SETUP | BLOCK_ON_GVFS_REPO },
+	{ "worktree", cmd_worktree, RUN_SETUP | BLOCK_ON_VFS_ENABLED },
 	{ "write-tree", cmd_write_tree, RUN_SETUP },
 };
 
