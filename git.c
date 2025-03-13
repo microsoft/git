d95b54f5e024ba1600dd0fae6e061c75bf68df8c
Matthew John Cheetham <mjcheetham@outlook.com>
2025-03-13 13:31:14 +0000

    git.c: permit fsck cmd in Scalar repos
    Loosen the blocking of the `fsck` command from all "GVFS repos" (those
    that have `core.gvfs` set) to only those that actually use the virtual
    file system (VFS for Git only). This allows for `fsck` to be used in
    Scalar clones.
    
    Signed-off-by: Matthew John Cheetham <mjcheetham@outlook.com>

--- git.c
+++ git.c
@@ -676,7 +676,7 @@ static struct cmd_struct commands[] = {
 	{ "for-each-repo", cmd_for_each_repo, RUN_SETUP_GENTLY },
 	{ "format-patch", cmd_format_patch, RUN_SETUP },
 	{ "format-rev", cmd_format_rev, RUN_SETUP },
-	{ "fsck", cmd_fsck, RUN_SETUP | BLOCK_ON_GVFS_REPO},
+	{ "fsck", cmd_fsck, RUN_SETUP | BLOCK_ON_VFS_ENABLED },
 	{ "fsck-objects", cmd_fsck, RUN_SETUP },
 	{ "fsmonitor--daemon", cmd_fsmonitor__daemon, RUN_SETUP },
 	{ "gc", cmd_gc, RUN_SETUP },
