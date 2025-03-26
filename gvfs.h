26503a2024da28ed6fce3487f98ad6843754cc02
Matthew John Cheetham <mjcheetham@outlook.com>
2025-03-26 10:31:38 +0000

    builtin/repack.c: emit warning when shared cache is present
    Emit a warning message when the `gvfs.sharedCache` option is set that
    the `repack` command will not perform repacking on the shared cache.
    
    In the future we can teach `repack` to operate on the shared cache, at
    which point we can drop this commit.
    
    Signed-off-by: Matthew John Cheetham <mjcheetham@outlook.com>

--- gvfs.h
+++ gvfs.h
@@ -30,6 +30,8 @@
 #define GVFS_BLOCK_FILTERS_AND_EOL_CONVERSIONS      (1 << 6)
 #define GVFS_PREFETCH_DURING_FETCH		    (1 << 7)
 
+#define GVFS_ANY_MASK                               0xFFFFFFFF
+
 void gvfs_load_config_value(const char *value);
 int gvfs_config_is_set(int mask);
 
