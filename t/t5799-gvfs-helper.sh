a5d50f021768edbdea41eff80a276d7d6b9f5206
Derrick Stolee <stolee@gmail.com>
2026-01-05 16:02:42 -0500

    gvfs-helper: override cache server for post
    This extension of the gvfs.cache-server config now allows a new key,
    gvfs.post.cache-server, to overrid the cache-server URL for only the
    batched objects endpoint.
    
    The purpose of this config is to allow for incremental testing and
    deployment of new cache-server infrastructure.
    
    Signed-off-by: Derrick Stolee <stolee@gmail.com>

--- t/t5799-gvfs-helper.sh
+++ t/t5799-gvfs-helper.sh
@@ -1773,4 +1773,53 @@ test_expect_success 'verb-specific cache-server: prefetch does NOT use gvfs.get.
 	verify_server_was_not_contacted 1
 '
 
+test_expect_success 'verb-specific cache-server: post uses gvfs.post.cache-server' '
+	test_when_finished "per_test_cleanup" &&
+	test_when_finished "git -C \"$REPO_T1\" config --unset gvfs.post.cache-server" &&
+	start_gvfs_protocol_server 0 &&
+	start_gvfs_protocol_server 1 &&
+
+	# Configure server 0 as default cache-server and server 1 for post.
+	git -C "$REPO_T1" config gvfs.cache-server "$(cache_server_url 0)" &&
+	git -C "$REPO_T1" config gvfs.post.cache-server "$(cache_server_url 1)" &&
+
+	# Run post - should go to server 1.
+	git -C "$REPO_T1" gvfs-helper \
+		--cache-server=trust \
+		--remote=origin \
+		--no-progress \
+		post \
+		<"$OIDS_BLOBS_FILE" >OUT.output 2>OUT.stderr &&
+
+	# Verify server 1 was contacted (post-specific).
+	verify_server_was_contacted 1 &&
+
+	# Verify server 0 was NOT contacted.
+	verify_server_was_not_contacted 0
+'
+
+test_expect_success 'verb-specific cache-server: get does NOT use gvfs.post.cache-server' '
+	test_when_finished "per_test_cleanup" &&
+	test_when_finished "git -C \"$REPO_T1\" config --unset gvfs.post.cache-server" &&
+	start_gvfs_protocol_server 0 &&
+	start_gvfs_protocol_server 1 &&
+
+	# Configure server 0 as default cache-server and server 1 for post.
+	git -C "$REPO_T1" config gvfs.cache-server "$(cache_server_url 0)" &&
+	git -C "$REPO_T1" config gvfs.post.cache-server "$(cache_server_url 1)" &&
+
+	# Run get - should go to server 0 (default), not server 1 (post).
+	git -C "$REPO_T1" gvfs-helper \
+		--cache-server=trust \
+		--remote=origin \
+		get \
+		<"$OID_ONE_BLOB_FILE" >OUT.output 2>OUT.stderr &&
+
+	# Verify server 0 was contacted (default cache-server).
+	verify_server_was_contacted 0 &&
+
+	# Verify server 1 was NOT contacted (post-specific).
+	verify_server_was_not_contacted 1
+'
+
 test_done
