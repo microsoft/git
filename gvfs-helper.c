3dc9149b3f628b900f6f558122774fa93ef20e10
Derrick Stolee <stolee@gmail.com>
2026-01-08 16:28:22 -0500

    gvfs-helper: emit advice on transient errors
    Users sometimes see transient network errors, but they are actually due
    to some other problem within the installation of a packfile. Observed
    resolutions include freeing up space on a full disk or deleting the
    shared object cache because something was broken due to a file
    corruption or power outage.
    
    However, when gvfs_advice_on_retry() fires after exhausting retries,
    it would be detrimental to suggest deleting the shared object cache
    when the failures are transient network or service errors (e.g.
    CURLE_RECV_ERROR, HTTP 429, HTTP 503) rather than local filesystem
    problems like index-pack failures. In this instance, it is better to
    check for network outages or ask for assistance (otherwise the user
    would not be able to redownload data and be stuck without an ability
    to work).
    
    This change only provides the advice to suggest those workarounds to
    help users help themselves.
    
    This is our first advice custom to the microsoft/git fork, so I have
    partitioned the key away from the others to avoid adjacent change
    conflicts (at least until upstream adds a new change at the end of the
    alphabetical list).
    
    We could consider providing a tool that does a more robust check of the
    shared object cache, but since 'git fsck' isn't safe to run as it may
    download missing objects, we do not have that ability at the moment.
    
    The good news is that it is safe to delete and rebuild the shared
    object cache as long as all local branches are pushed and network
    connectivity is healthy. The branches must be pushed because the
    local .git/objects/ directory is moved to the shared object cache in the
    'cache-local-objects' maintenance task.
    
    Signed-off-by: Derrick Stolee <stolee@gmail.com>
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- gvfs-helper.c
+++ gvfs-helper.c
@@ -255,6 +255,7 @@
 #include "packfile.h"
 #include "date.h"
 #include "versioncmp.h"
+#include "advice.h"
 
 #define TR2_CAT "gvfs-helper"
 
@@ -326,12 +327,15 @@ enum gh__error_code {
 	GH__ERROR_CODE__HTTP_503 = 6,
 	GH__ERROR_CODE__HTTP_OTHER = 7,
 	GH__ERROR_CODE__UNEXPECTED_CONTENT_TYPE = 8,
-	GH__ERROR_CODE__COULD_NOT_CREATE_TEMPFILE = 8,
-	GH__ERROR_CODE__COULD_NOT_INSTALL_LOOSE = 10,
-	GH__ERROR_CODE__COULD_NOT_INSTALL_PACKFILE = 11,
-	GH__ERROR_CODE__SUBPROCESS_SYNTAX = 12,
-	GH__ERROR_CODE__INDEX_PACK_FAILED = 13,
-	GH__ERROR_CODE__COULD_NOT_INSTALL_PREFETCH = 14,
+
+	GH__ERROR_CODE__HTTP_ERROR_LIMIT = 9,
+
+	GH__ERROR_CODE__COULD_NOT_CREATE_TEMPFILE = 10,
+	GH__ERROR_CODE__COULD_NOT_INSTALL_LOOSE = 11,
+	GH__ERROR_CODE__COULD_NOT_INSTALL_PACKFILE = 12,
+	GH__ERROR_CODE__SUBPROCESS_SYNTAX = 13,
+	GH__ERROR_CODE__INDEX_PACK_FAILED = 14,
+	GH__ERROR_CODE__COULD_NOT_INSTALL_PREFETCH = 15,
 };
 
 enum gh__cache_server_mode {
@@ -3016,6 +3020,41 @@ static int compute_transient_delay(int attempt)
 	return v;
 }
 
+static void gvfs_advice_on_retry(enum gh__error_code ec)
+{
+	static int advice_given = 0;
+
+	if (advice_given)
+		return;
+	advice_given = 1;
+
+	if (ec < GH__ERROR_CODE__HTTP_ERROR_LIMIT) {
+		advise_if_enabled(ADVICE_GVFS_HELPER_TRANSIENT_RETRY,
+				  "GVFS Protocol network requests are failing. This is\n"
+				  "likely caused by a service outage or unstable network\n"
+				  "connection. Check your local network or contact your\n"
+				  "engineering systems team for assistance.");
+		return;
+	}
+
+	if (gvfs_shared_cache_pathname.len) {
+		advise_if_enabled(ADVICE_GVFS_HELPER_TRANSIENT_RETRY,
+				  "These retries may hint towards issues with your disk or\n"
+				  "shared object cache. Check to see if your disk is full.\n"
+				  "If your disk has space, then your shared object cache\n"
+				  "may have corrupt files. Push all local branches then\n"
+				  "delete '%s'\n"
+				  "and run 'git fetch' to reload the cache.",
+				  gvfs_shared_cache_pathname.buf);
+	} else {
+		advise_if_enabled(ADVICE_GVFS_HELPER_TRANSIENT_RETRY,
+				  "These retries may hint towards issues with your disk.\n"
+				  "Check to see if your disk is full. Note also that you\n"
+				  "do not have a gvfs.sharedCache config, which is not\n"
+				  "normal. You may need to delete and reclone this repo.");
+	}
+}
+
 /*
  * Robustly make an HTTP request.  Retry if necessary to hide common
  * transient network errors and/or 429 blockages.
@@ -3058,6 +3097,10 @@ static void do_req__with_robust_retry(const char *url_base,
 			/*fallthru*/
 
 		case GH__RETRY_MODE__TRANSIENT:
+			/*
+			 * Give advice for common reasons this could happen:
+			 */
+			gvfs_advice_on_retry(status->ec);
 			params->k_transient_delay_sec =
 				compute_transient_delay(params->k_attempt);
 			continue;
